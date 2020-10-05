f244309ae1737d98af78778019b7bc3074500fd2
Ellaine Tolentino <rckrleyn@gmail.com>
2020-10-05 23:32:41 +0000

    Automatically backed up by Learn

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -192,29 +192,29 @@ end
 
 
 
-# If you'd like to work on the bonus, uncomment these tests.
-# describe 'bonus' do
-#   describe '#most_points_scored' do
-#     it 'returns Ben Gordon' do
-#       expect(most_points_scored).to eq("Ben Gordon")
-#     end
-#   end
-#   describe '#winning_team' do
-#     it 'returns the Brooklyn Nets' do
-#       expect(winning_team).to eq("Brooklyn Nets")
-#     end
-#   end
-#   describe '#player_with_longest_name' do
-#     it 'returns Brendan Haywood' do
-#       expect(player_with_longest_name).to eq("Brendan Haywood")
-#     end
-#   end
-# end
-# describe 'super bonus' do
-#   describe '#long_name_steals_a_ton?' do
-#     it 'returns true' do
-#       expect(long_name_steals_a_ton?).to eq(true)
-#     end
-#   end
-# end
+If you'd like to work on the bonus, uncomment these tests.
+describe 'bonus' do
+  describe '#most_points_scored' do
+    it 'returns Ben Gordon' do
+      expect(most_points_scored).to eq("Ben Gordon")
+    end
+  end
+  describe '#winning_team' do
+    it 'returns the Brooklyn Nets' do
+      expect(winning_team).to eq("Brooklyn Nets")
+    end
+  end
+  describe '#player_with_longest_name' do
+    it 'returns Brendan Haywood' do
+      expect(player_with_longest_name).to eq("Brendan Haywood")
+    end
+  end
+end
+describe 'super bonus' do
+  describe '#long_name_steals_a_ton?' do
+    it 'returns true' do
+      expect(long_name_steals_a_ton?).to eq(true)
+    end
+  end
+end
 
